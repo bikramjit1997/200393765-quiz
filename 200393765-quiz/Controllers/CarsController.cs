406220b274ab78ca5408bc819bbed44a34f6f535
Bikramjit Singh <200393765@student.georgianc.on.ca>
2019-05-29 23:51:38 -0400

    Code edit

---
+++ 200393765-quiz/Controllers/CarsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _200393765_quiz.Models;
+
+namespace _200393765_quiz.Controllers
+{
+    public class CarsController : Controller
+    {
+        //Cars List
+        List<Car> carList = new List<Car>()
+        {
+            new Car {ID = 1, Brand = "Toyota", Model = "Corolla", Type = "Compact", Year = 2017},
+            new Car {ID=2, Brand="Ford", Model="F-150", Type="Truck", Year=2018},
+            new Car {ID=3, Brand="Dodge", Model="Caravan", Type="Minivan", Year=2019}
+        };
+        // GET: Cars
+        public ActionResult Index()
+        {
+            return View(carList);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null || id > carList.Count)
+            {
+                return Content("Invalid Car ID");
+            }
+            var ind = Convert.ToInt32(id) - 1;
+            var car = carList[ind];
+
+            return View(car);
+        }
+    }
+}
---
+++ 200393765-quiz/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200393765_quiz.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+    }
+}
