e4cb5112a7a009aa367104ae2a72f6a83895a0e0
Bikramjit Singh <200393765@student.georgianc.on.ca>
2019-05-29 23:35:09 -0400

    Add project files.

---
+++ 200393765-quiz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_200393765_quiz.Startup))]
+namespace _200393765_quiz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
